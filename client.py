3e123c64ec233163dc413e3d1d5722104adf0cba
Ryan Goluch <rgoluch@iastate.edu>
2020-03-26 13:00:11 -0500

    cleaning up video stream src

--- client.py
+++ client.py
@@ -22,8 +22,7 @@ sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
-vs = VideoStream(usePiCamera=True).start()
-#vs = VideoStream(src=0).start()
+vs = VideoStream(src="http://192.168.1.122/video.mjpg").start()
 time.sleep(2.0)
  
 while True:
