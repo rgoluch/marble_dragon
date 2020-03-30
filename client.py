1a9a7e14e6aeb387d5214a9ac6ef0fd41eba351e
rgoluch <rgoluch@iastate.edu>
2020-03-29 19:56:56 -0500

    fixing remote timeout issue

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import imagezmq
 import argparse
 import socket
 import time
+import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -17,16 +18,21 @@ args = vars(ap.parse_args())
 
 # initialize the ImageSender object with the socket address of the
 # server
+
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
 	args["server_ip"]))
 
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
 rpiName = socket.gethostname()
-vs = VideoStream(src="http://"+str(args["camera_ip"])+"/video.mjpg").start()
+feed = cv2.VideoCapture("http://" + str(args["camera_ip"]) + "/video.mjpg")
+# ret, img = feed.read()
+# vs = VideoStream(src="http://"+str(args["camera_ip"])+"/video.mjpg").start()
 time.sleep(2.0)
- 
+i = 0
 while True:
 	# read the frame from the camera and send it to the server
-	frame = vs.read()
+	ret, frame = feed.read()
+	print(i)
+	i+=1
 	sender.send_image(rpiName, frame)
